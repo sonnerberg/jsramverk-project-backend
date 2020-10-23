2a7a844724ed38d92f342423d28c24e102854d12
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2020-10-23 17:07:32 +0200

    Fix getStockPrice

--- src/util/stock.js
+++ src/util/stock.js
@@ -1,7 +1,7 @@
 var stock = {
   randomAroundZero: () => (Math.random() > 0.5 ? 1 : -1),
-  getStockPrice: ({ start, rate, variance }) =>
-    start * rate + variance * stock.randomAroundZero(),
+  getStockPrice: ({ startingPoint, rate, variance }) =>
+    startingPoint * rate + variance * stock.randomAroundZero(),
 }
 
 module.exports = stock
