d337b705678219750e2b2db2bcf99ffc14298c76
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2020-10-26 17:38:35 +0100

    Update stock util

--- src/util/stock.js
+++ src/util/stock.js
@@ -1,7 +1,15 @@
 var stock = {
   randomAroundZero: () => (Math.random() > 0.5 ? 1 : -1),
-  getStockPrice: ({ startingPoint, rate, variance }) =>
-    startingPoint * rate + variance * stock.randomAroundZero(),
+  getStockPrice: ({ startingPoint, rate, variance }) => {
+    let randomNumber = Number(
+      startingPoint * rate + variance * stock.randomAroundZero()
+    ).toFixed(2)
+
+    if (randomNumber > 0) {
+      return randomNumber
+    }
+    return -randomNumber
+  },
 }
 
 module.exports = stock
